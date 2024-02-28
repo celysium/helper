0ee1004867d2a18a3e05c6a949cac0ab4fbf5233
nasser <hekmati.nasser@gmail.com>
2024-02-28 12:52:01 +0330

    fix: filter and interface

--- src/Repository/BaseRepository.php
+++ src/Repository/BaseRepository.php
@@ -1,10 +1,12 @@
 <?php
 
-namespace Celysium\Base\Repository;
+namespace Celysium\Helper\Repository;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Request;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Collection;
 
@@ -16,17 +18,17 @@ class BaseRepository implements BaseRepositoryInterface
     {
     }
 
-    public function index(array $parameters = [], array $columns = ['*']): LengthAwarePaginator|Collection
+    public function index(Request $request, array $columns = ['*']): LengthAwarePaginator|Collection
     {
         $query = $this->model->query();
 
-        $query = $this->query($query, $parameters);
+        $query = $this->query($query, $request);
 
-        $query = $this->filters($query, $parameters);
+        $query = $this->filters($query, $request);
 
-        $query = $this->sort($query, $parameters);
+        $query = $this->sort($query, $request);
 
-        return $this->export($query, $parameters, $columns);
+        return $this->export($query, $request, $columns);
     }
 
     public function conditions(Builder $query): array
@@ -34,46 +36,47 @@ class BaseRepository implements BaseRepositoryInterface
         return [];
     }
 
-    protected function filters(Builder $query, array $parameters = [], array $columns = ['*']): Builder
+    private function filters(Builder $query, Request $request): Builder
     {
-        $this->columns = $columns;
-        $rules = $this->conditions($query);
-        foreach ($rules as $field => $condition) {
-            if (array_key_exists($field, $parameters)) {
-                if (is_callable($condition)) {
-                    $query = $condition($parameters[$field]);
-                } elseif ($condition == 'like') {
-                    $query->where($field, $condition, "%" . $parameters[$field] . "%");
-                } else {
-                    $query->where($field, $condition, $parameters[$field]);
-                }
+        $parameters = $request->all();
+        $conditions = $this->conditions($query);
+        if (count($parameters) == 0 || count($conditions) == 0 || count($commons = array_intersect(array_keys($parameters), array_keys($conditions))) == 0) {
+            return $query;
+        }
+        foreach ($commons as $field) {
+            if (is_callable($conditions[$field])) {
+                $query = $conditions[$field]($parameters[$field]);
+            } elseif ($conditions[$field] == 'like') {
+                $query->where($field, 'like', "%" . $parameters[$field] . "%");
+            } else {
+                $query->where($field, $conditions[$field], $parameters[$field]);
             }
         }
         return $query;
     }
 
-    public function query(Builder $query, array $parameters): Builder
+    public function query(Builder $query, Request $request): Builder
     {
         return $query;
     }
 
-    protected function sort(Builder $query, array $parameters): Builder
+    protected function sort(Builder $query, Request $request): Builder
     {
         return $query
-            ->orderBy($parameters['sort_by'] ?? $this->model->getKeyName(), $parameters['sort_direction'] ?? 'desc');
+            ->orderBy($request['sort_by'] ?? $this->model->getKeyName(), $request['sort_direction'] ?? 'desc');
     }
 
-    protected function export(Builder $query, array $parameters = [], array $columns = ['*']): Builder|Collection|LengthAwarePaginator|array
+    protected function export(Builder $query, Request $request, array $columns = ['*']): Builder|Collection|LengthAwarePaginator|array
     {
         if ($columns != $this->columns) {
             $this->columns = $columns;
         }
 
-        return match ($parameters['export_type'] ?? null) {
-            'builder'    => $query->select($columns),
+        return match ($request['export_type'] ?? null) {
+            'builder' => $query->select($columns),
             'collection' => $query->get($columns),
-            'array'      => $query->get($columns)->toArray(),
-            default      => $query->paginate($parameters['per_page'] ?? $this->model->getPerPage(), $columns)
+            'array' => $query->get($columns)->toArray(),
+            default => $query->paginate($request['per_page'] ?? $this->model->getPerPage(), $columns)
         };
     }
 
@@ -92,35 +95,30 @@ class BaseRepository implements BaseRepositoryInterface
         return $this->model->query()->findOrFail($id);
     }
 
-    public function store(array $parameters): Model
+    public function store(FormRequest $request): Model
     {
         return $this->model->query()
-            ->create($parameters);
+            ->create($request->validated());
     }
 
-    public function update(Model $model, array $parameters): Model
+    public function update(Model $model, FormRequest $request): Model
     {
-        $model->update($parameters);
+        $model->update($request->validated());
 
         return $model->refresh();
     }
 
-    public function updateById(int|string $id, array $parameters): Model
+    public function updateById(int|string $id, FormRequest $request): Model
     {
         $result = $this->model->query()
             ->where($this->model->getKeyName(), $id)
-            ->update($parameters);
+            ->update($request->validated());
 
         if ($result === 0) {
             throw (new ModelNotFoundException)->setModel(get_class($this->model), [$id]);
         }
 
         return $this->find($id);
-    }
-
-    public function updateOrCreate(Model $model, array $conditions, array $values): Model
-    {
-        return $model->query()->updateOrCreate($conditions, $values);
     }
 
     public function destroy(Model $model): bool
--- src/Repository/BaseRepositoryInterface.php
+++ src/Repository/BaseRepositoryInterface.php
@@ -1,18 +1,20 @@
 <?php
-namespace Celysium\Base\Repository;
+namespace Celysium\Helper\Repository;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Request;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Collection;
 
 interface BaseRepositoryInterface
 {
-    public function index(array $parameters = [], array $columns = ['*']): LengthAwarePaginator|Collection;
+    public function index(Request $request, array $columns = ['*']): LengthAwarePaginator|Collection;
 
     public function conditions(Builder $query): array;
 
-    public function query(Builder $query, array $parameters): Builder;
+    public function query(Builder $query, Request $request): Builder;
 
     public function show(Model $model): Model;
 
@@ -20,13 +22,11 @@ interface BaseRepositoryInterface
 
     public function findOrFail(int|string $id): ?Model;
 
-    public function store(array $parameters): Model;
+    public function store(FormRequest $request): Model;
 
-    public function update(Model $model, array $parameters): Model;
+    public function update(Model $model, FormRequest $request): Model;
 
-    public function updateById(int|string $id, array $parameters): ?Model;
-
-    public function updateOrCreate(Model $model, array $conditions, array $values): Model;
+    public function updateById(int|string $id, FormRequest $request): ?Model;
 
     public function destroy(Model $model): bool;
 
