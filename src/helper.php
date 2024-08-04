6d82cf4b37c7d0f00f2837087aef8986729914e5
Nasser Hekmati <hekmati.nasser@gmail.com>
2024-08-04 15:49:47 +0330

    fix: remove storage functions

--- src/helper.php
+++ src/helper.php
@@ -66,17 +66,3 @@ if (!function_exists('regularIranianMobile')) {
         return $mobile;
     }
 }
-
-if (!function_exists('storageStore')) {
-    function storageStore(\Psr\Http\Message\StreamInterface|\Illuminate\Http\File|\Illuminate\Http\UploadedFile|string $contents): string|false
-    {
-        return \Illuminate\Support\Facades\Storage::put(now()->format('Y/n/j'), $contents);
-    }
-}
-
-if (!function_exists('storageDelete')) {
-    function storageDelete(array|string $paths): string|false
-    {
-        return \Illuminate\Support\Facades\Storage::delete($paths);
-    }
-}
